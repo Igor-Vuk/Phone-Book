1e9d2559cd0abb50dcc1abb6364b2a244281e0b9
Igor Vukelić <igvukelic@gmail.com>
2017-02-27 04:35:43 +0100

    configure mLab

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,5 @@
 var webpack = require("webpack");
 var path = require("path");
-var envFile = require("node-env-file");
-
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
-try {
-    envFile(path.join(__dirname, "config/" + process.env.NODE_ENV + ".env"));
-} catch (e) {
-
-}
 
 module.exports = {
     entry: [
@@ -24,18 +15,7 @@ module.exports = {
             "$": "jquery",
             "jQuery": "jquery",
             "jquery": "jquery"
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false
-            }
-        }),
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-                DATABASE_URL: JSON.stringify(process.env.DATABASE_URL)
-            }
-        })    
+        }),   
     ],
     output: {
         path: __dirname,
