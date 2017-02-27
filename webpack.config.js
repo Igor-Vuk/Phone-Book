630f7c647a3c65c81f09ca4c124e437e2096c02a
Igor Vukelić <igvukelic@gmail.com>
2017-02-27 05:01:44 +0100

    add bundle file

--- webpack.config.js
+++ webpack.config.js
@@ -57,5 +57,5 @@ module.exports = {
             path.resolve(__dirname, "./node_modules/foundation-sites/scss")
         ]
     },
-    devtool: process.env.NODE_ENV === "production" ? undefined : "#source-maps"
+    devtool: "#source-maps"
 };
